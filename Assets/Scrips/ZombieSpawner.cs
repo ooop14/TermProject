4507037b4d14820c5bbcffd95e6f846ee68ce822
Heejo <ihejo12345@gmail.com>
2025-09-16 01:33:17 +0900

    좀비 속도와 랜덤 속도

--- Assets/Scrips/ZombieSpawner.cs
+++ Assets/Scrips/ZombieSpawner.cs
@@ -1,6 +1,19 @@
+using System;
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class ZombieSpawner : MonoBehaviour
 {
-    
+    public Transform[] spawnpoints;
+
+    public GameObject zombie;
+
+    private void Start(){
+        InvokeRepeating("Spawnzombie", 2, 5);
+    }
+
+    void Spawnzombie()
+    {
+        int r = UnityEngine.Random.Range(0, spawnpoints.Length);
+        GameObject myzombie = Instantiate(zombie, spawnpoints[r].position,Quaternion.identity);
+    }
 }
