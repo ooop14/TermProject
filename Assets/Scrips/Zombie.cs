a80f5d3103c59da3320c28c784ceba8497685853
Heejo <ihejo12345@gmail.com>
2025-09-16 00:31:23 +0900

    zombie speed

--- Assets/Scrips/Zombie.cs
+++ Assets/Scrips/Zombie.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class Zombie
+public class Zombie : MonoBehaviour
 {
-    
+    public int health;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() { 
+        transform.position -= new Vector3(speed, 0, 0);
+    }
 }
